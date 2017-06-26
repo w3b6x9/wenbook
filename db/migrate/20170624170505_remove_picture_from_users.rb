676c699cb8d7d122bfa875bf7153b6ccc3277da2
Wen Bo Xie <wenbox323@gmail.com>
2017-06-26 15:22:45 -0400

    Delete migrate file

--- db/migrate/20170624170505_remove_picture_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemovePictureFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :picture_file_name, :string
-    remove_column :users, :picture_content_type, :string
-    remove_column :users, :picture_file_size, :integer
-    remove_column :users, :picture_updated_at, :datetime
-  end
-end
