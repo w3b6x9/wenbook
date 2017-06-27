96347bcd606f9f0bea2cd2161ac5ffdbe1c1b71e
Wen Bo Xie <wenbox323@gmail.com>
2017-06-26 23:13:29 -0400

    Style friend requests section

--- frontend/components/friendship_request_page/pending_requests.jsx
+++ frontend/components/friendship_request_page/pending_requests.jsx
@@ -19,23 +19,36 @@ export default class PendingRequests extends React.Component {
   render() {
     const requests = this.props.pending_requests.map(request => {
       return (
-        <li key={request.friendship_id}>
-          {request.first_name}
-          <button onClick={this.handleClick(
-              'confirmed',
-              request.friendship_id
-            )}>Confirm</button>
-          <button onClick={this.handleClick(
-              'denied',
-              request.friendship_id
-            )}>Delete Request</button>
+        <li key={request.friendship_id} className='request-item'>
+          <div className='info-left'>
+            <img src={request.profile_picture} />
+            <div className='user-name'>
+              { request.first_name + ' ' + request.last_name }
+            </div>
+          </div>
+          <div className='action-btn'>
+            <button className='confirm-btn btn' onClick={this.handleClick(
+                'confirmed',
+                request.friendship_id
+              )}>Confirm</button>
+            <button className='delete-btn btn' onClick={this.handleClick(
+                'denied',
+                request.friendship_id
+              )}>Delete Request</button>
+          </div>
         </li>
       );
     });
+    const requestsCount = this.props.pending_requests.length;
 
     return (
-      <div>
-        { requests }
+      <div className='requests-container'>
+        <div className='requests-header'>
+          Respond to Your { requestsCount } Friend Requests
+        </div>
+        <ul className='request-list'>
+          { requests }
+        </ul>
       </div>
     );
   }
