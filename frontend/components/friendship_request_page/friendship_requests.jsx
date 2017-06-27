a0aab82bc4d71244f0f9cf0444a66b7b7ac2363a
Wen Bo Xie <wenbox323@gmail.com>
2017-06-26 23:13:29 -0400

    Style friend requests section

--- frontend/components/friendship_request_page/friendship_requests.jsx
+++ frontend/components/friendship_request_page/friendship_requests.jsx
@@ -4,7 +4,7 @@ import PendingRequestsContainer from './pending_requests_container';
 export default class FriendshipRequests extends React.Component {
   render() {
     return (
-      <div>
+      <div className='friend-requests-main'>
         <PendingRequestsContainer />
       </div>
     );
