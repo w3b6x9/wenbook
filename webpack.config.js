008901a8fcdc01a6ace1fee369a6d4b9f7db01b6
Wen Bo Xie <wenbox323@gmail.com>
2017-07-03 13:01:50 -0400

    Setup app for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 const path = require("path");
+const webpack = require("webpack");
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
 
 module.exports = {
   context: __dirname,
