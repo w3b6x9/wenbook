32ac001acc7e7abd47056bb7ec352f2c4f10f16e
Wen Bo Xie <wenbox323@gmail.com>
2017-07-18 22:27:14 -0400

    Update email form on about page

--- frontend/components/detail_page/email_form.jsx
+++ frontend/components/detail_page/email_form.jsx
@@ -15,20 +15,30 @@ export default class EmailForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     this.props.updateSingleUser(this.state);
+    this.props.closeForm('emailForm')();
   }
 
   render() {
     return (
-      <form onSubmit={ this.handleSubmit }>
-        <div>
-          <input className='input-box'
-            type='text'
-            onChange={ this.handleChange('email') }
-            value={ this.state.email }
-          />
-        </div>
-        <div>
-          <input type='submit' value='Save changes' />
+      <form onSubmit={ this.handleSubmit } className='detail-name-form'>
+
+        <div className='detail-name-input'>
+          <div className='form-top-row'>
+            <label className='detail-field-row'>
+              <div className='detail-name-title'>Email</div>
+              <input className='detail-input-box'
+                type='text'
+                onChange={ this.handleChange('email') }
+                value={ this.state.email }
+              />
+            </label>
+            <div className='cancel-btn' onClick={this.props.closeForm('emailForm')}>
+              <i className="fa fa-times cancel-icon" aria-hidden="true" />
+              Cancel
+            </div>
+          </div>
+          <input className='confirm-btn detail-submit'
+            type='submit' value='Save changes' />
         </div>
       </form>
     )
